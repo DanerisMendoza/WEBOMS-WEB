9430387b869ba2b6260c5e7444faedb1b902878a
dane <danerismendoza096@gmail.com>
2022-08-28 16:53:08 +0800

    v3

--- admin.php
+++ admin.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,7 +6,8 @@
 </head>
 <body>
   <div class="container text-center">
-    <button type="button" class="btn" data-toggle="modal" data-target="#loginModal">Add new dish</button>
+    <button id="addButton" type="button" class="btn" data-toggle="modal" data-target="#loginModal">Add new dish</button>
+
     <button class="btn" id="Logout">Logout</button>
     <script>document.getElementById("Logout").onclick = function () {window.location.replace('login.php'); };</script>
 		<div class="col-lg-12">
@@ -35,39 +35,39 @@
           <td><?=$rows['dish']?></td>
           <td><?php echo $rows['cost']; ?></td>
 				  <td><?php $pic = $rows['picName']; echo "<img src='dishesPic/$pic' style=width:100px;height:100px>";?></td>
-				  <td><a href="?idAndPicname=<?php echo $rows['ID']." ".$rows['picName'] ?>">Delete</a></td>
-          <td><a href="update.php?ID=<?php echo $rows['ID'] ?>">Update</a></td>
+				  <td><a href="?idAndPicnameDelete=<?php echo $rows['ID']." ".$rows['picName'] ?>">Delete</a></td>
+				  <td ><a href="updatePage.php?idAndPicnameUpdate=<?php echo $rows['ID']." ".$rows['dish']." ".$rows['cost']." ".$rows['picName']." update" ?>"  >Update</a></td>
 			    </tr>
-
 			    <?php } ?>
 			  </tbody>
 			</table>
 			<?php } ?>
 		</div>
-	<!-- <div class="col-lg-6" id="piechart" style="width: 900px; height: 500px;"></div> -->
-	
-    
   </div>
-
+  
   <div class="modal fade" role="dialog" id="loginModal">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-body">
+            <div class="modal-body ">
                 <form method="post" class="form-group" enctype="multipart/form-data">
                     <input type="text" class="form-control" name="dishes" placeholder="dishes">
-                    <input type="text" class="form-control" name="cost" placeholder="cost">
+                    <input type="number" class="form-control" name="cost" placeholder="cost">
                     <input type="file"  name="fileInput">
                     <button type="submit" class="btn-success col-sm-12" name="insert">insert</button>
                 </form>
                 <?php
-                if (isset($_GET['idAndPicname']))   
-                  delete($_GET['idAndPicname'],$conn);
+                //use $_post when using method post otherwise use $_get
+                               
+                //calling delete function
+                if (isset($_GET['idAndPicnameDelete']))   
+                  delete($_GET['idAndPicnameDelete'],$conn);
+
+                //insert
                 if(isset($_POST['insert'])){
                 $dishes = $_POST['dishes'];
                 $cost = $_POST['cost'];
                 if(empty($dishes) || empty($cost))
                      echo "<script>alert('Please complete the details!'); window.location.replace('admin.php');</script>";
-                //add to db
                 include_once('connection.php');
                 $file = $_FILES['fileInput'];
                 $fileName = $_FILES['fileInput']['name'];
@@ -77,7 +77,7 @@
                 $fileType = $_FILES['fileInput']['type'];
                 $fileExt = explode('.',$fileName);
                 $fileActualExt = strtolower(end($fileExt));
-                $allowed = array('jpg','jpeg','png','pdf','docx');
+                $allowed = array('jpg','jpeg','png');
                 if(in_array($fileActualExt,$allowed)){
                     if($fileError === 0){
                         if($fileSize < 10000000){
@@ -123,20 +123,19 @@
 </style>
 
 <?php 
-  //functions
-  function delete($idAndPicname,$conn)
-    {
-		//converting variable to array using explode by whitespace
-		$idAndPicArr = explode(' ',$idAndPicname);
-		echo $idAndPicArr[1];
-		$sql = "DELETE FROM dishes_tb
-					WHERE id=$idAndPicArr[0]";
-		$result = mysqli_query($conn, $sql);
-		if (!$result){
-      echo "<script>alert('Delete data unsuccessfully'); window.location.replace('admin.php');</script>";  
-      echo "<script> window.location.replace('admin.php');</script>";
-    }
-    unlink("dishespic/".$idAndPicArr[1]);
-    echo "<script> window.location.replace('admin.php'); alert('Delete data successfully'); </script>";  
-    }
+  //delete
+  function delete($idAndPicname,$conn){
+  $idAndPicArr = explode(' ',$idAndPicname);
+  echo $idAndPicArr[1];
+  $sql = "DELETE FROM dishes_tb
+        WHERE id=$idAndPicArr[0]";
+  $result = mysqli_query($conn, $sql);
+  if (!$result){
+    echo "<script>alert('Delete data unsuccessfully'); window.location.replace('admin.php');</script>";  
+    echo "<script> window.location.replace('admin.php');</script>";
+  }
+  unlink("dishespic/".$idAndPicArr[1]);
+  echo "<script> window.location.replace('admin.php'); alert('Delete data successfully'); </script>";  
+}
 ?>
+
---
+++ updatePage.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html><head>
+<title></title>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css"></head>
+<body>
+<div class="container text-center">
+<?php
+    $idAndPicname = explode(' ',$_GET['idAndPicnameUpdate']);    
+    $id = $idAndPicname[0];
+    $dish = $idAndPicname[1];
+    $cost = $idAndPicname[2];
+    $picName = $idAndPicname[3];
+    echo "</br>dish: ".$dish."</br>cost: ".$cost."</br>picname: ".$picName."<br></br>";
+?>
+ <form method="post" class="form-group" enctype="multipart/form-data">
+<input type="text" class="form-control" name="dish" placeholder="dish"></br>
+<input type="number" class="form-control" name="cost" placeholder="cost"></br>
+<input type="file"  name="fileInput"></br>
+<button type="button" class="btn-success col-sm-12" id="cancel">Cancel</button>
+<button type="submit" class="btn-success col-sm-12" name="update">Update</button>
+<?php
+    if(isset($_POST['update'])){
+        $dish = $_POST['dish'];
+        $cost = $_POST['cost'];
+        if(empty($dish) || empty($cost) || $_FILES['fileInput']['name']==''){
+        echo "<script>alert('Please complete the details! ');</script>";
+        return;    
+        }
+        include_once('connection.php');
+        $file = $_FILES['fileInput'];
+        $fileName = $_FILES['fileInput']['name'];
+        $fileTmpName = $_FILES['fileInput']['tmp_name'];
+        $fileSize = $_FILES['fileInput']['size'];
+        $fileError = $_FILES['fileInput']['error'];
+        $fileType = $_FILES['fileInput']['type'];
+        $fileExt = explode('.',$fileName);
+        $fileActualExt = strtolower(end($fileExt));
+        $allowed = array('jpg','jpeg','png');
+        if(in_array($fileActualExt,$allowed)){
+            if($fileError === 0){
+                if($fileSize < 10000000){
+                    $fileNameNew = uniqid('',true).".".$fileActualExt;
+                    $fileDestination = 'dishesPic/'.$fileNameNew;
+                    move_uploaded_file($fileTmpName,$fileDestination);         
+                    $updateQuery = "UPDATE dishes_tb
+                    SET dish='$dish', 
+                    cost='$cost',
+                    picName = '$fileNameNew'
+                    WHERE id=$id ";        
+                    if(mysqli_query($conn,$updateQuery)){
+                        echo '<script>alert("Sucess updating the database!");</script>';       
+                        unlink("dishespic/".$picName);                                        
+                    }
+                    else{
+                        echo '<script type="text/javascript">alert("failed to save to database");</script>';  
+                    }
+                    echo "<script>window.location.replace('admin.php')</script>";                                
+                }
+                else
+                    echo "your file is too big!";
+            }
+            else
+                echo "there was an error uploading your file!";
+        }
+        else
+            echo "you cannot upload files of this type";  
+
+        $result = mysqli_query($conn, $sql);
+        if ($result) {
+            header("Location: ../read.php?success=successfully updated");
+        }else {
+            header("Location: ../update.php?id=$id&error=unknown error occurred&$user_data");
+        }
+    }
+?>
+ </form>
+</div>
+</body>
+</html>
+
+<script>
+    document.getElementById("cancel").addEventListener("click",function(){
+        window.location.replace('admin.php');
+    });
+</script>
