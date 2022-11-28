13afce3a4dcb6bd8eca31687eb51e1c7a52fc779
nicollette porca <20200108m.porca.nicollette.bscs@gmail.com>
2022-11-28 13:50:20 +0800

    index

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
-    header("location:Login.php");
+    // header("location:Login.php");
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RestoName</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+    <div class="container">
+        <a href="login.php" type="button">Login</a>
+    </div>
+
+</body>
+
+</html>
