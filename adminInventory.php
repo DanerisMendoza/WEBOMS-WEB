1970fa92c001ebf36ff0cd310f2a5949ad9cef44
nicollette porca <20200108m.porca.nicollette.bscs@gmail.com>
2022-12-05 14:22:21 +0800

    title tab fix

--- adminInventory.php
+++ adminInventory.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Inventory</title>
+    <title>Inventory</title>
 
     <link rel="stylesheet" href="css/bootstrap 5/bootstrap.min.css">
     <link rel="stylesheet" href="css/admin.css">
