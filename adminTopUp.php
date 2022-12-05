e8351160b78883ad8b24fdcbe06199dea269a87f
nicollette porca <20200108m.porca.nicollette.bscs@gmail.com>
2022-12-05 14:56:13 +0800

    admin top up ui

--- adminTopUp.php
+++ adminTopUp.php
@@ -3,100 +3,243 @@
   include('method/checkIfAccountLoggedIn.php');
   include_once('method/query.php');
 ?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Costumer - TopUp List</title>
-        
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <script type="text/javascript" src="js/jquery-3.6.1.min.js"></script>  
+    <title>Admin</title>
+
+    <link rel="stylesheet" href="css/bootstrap 5/bootstrap.min.css">
+    <link rel="stylesheet" href="css/admin.css">
+    <script type="text/javascript" src="js/bootstrap 5/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery-3.6.1.min.js"></script>
+    <!-- online css bootsrap icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
-    
 </head>
-<body class="bg-light">
-<div class="container text-center mt-5">
-    <button class="btn btn-lg btn-dark col-12 mb-4" id="back">Back</button>
-          <script>
-              document.getElementById("back").onclick = function () {window.location.replace('admin.php');};    
-          </script> 
-              <?php
-              $query = "SELECT a.*,b.name FROM `WEBOMS_topUp_tb` a inner join WEBOMS_userInfo_tb b on a.user_id = b.user_id";
-              $resultSet =  getQuery($query);
-              ?>
-              <div class="table-responsive col-lg-12">
-              <table class="table table-striped table-bordered mb-5 col-lg-12">
-              <thead class="table-dark">
-                  <tr>	
-                  <th scope="col">NAME</th>
-                  <th scope="col">Amount</th>
-                  <th scope="col">Proof Of Payment</th>
-                  <th scope="col">Status</th>
-                  <th scope="col">Date</th>
-                  <th scope="col" colspan="3">Action</th>
-                  </tr>
-              </thead>
-                <tbody>
-                  <?php
-                  if($resultSet!= null)
-                  foreach($resultSet as $row){ ?>
-                  <tr>	   
-                    <td><?php echo $row['name']; ?></td>
-                    <td><?php echo '₱'.$row['amount'];?></td>
-                    <td><?php echo $row['proofOfPayment'];?></td>
-                    <td><?php echo $row['status'];?></td>
-                    <td><?php echo date('m/d/Y h:i a ', strtotime($row['date']));?></td>
-                    <td><a class="btn btn-primary border-dark" href="?viewPic=<?php echo $row['proofOfPayment'];?>">View</a></td>
-                        <?php if($row['status'] == 'pending') {?>
-                        <td><a class="btn btn-success border-dark" href="?approve=<?php echo $row['id'].','.$row['user_id'].','.$row['amount'];?>">Approve</a></td>
-                        <td><a class="btn btn-danger border-dark" href="?disapprove=<?php echo $row['id'];?>">Disapprove</a></td>
-                        <?php } else {?>
-                        <td colspan="3">none</td>
-                        <?php } ?>
-                    </tr>
-                  <?php } ?>
-                </tbody>
-              </table>
-          </div>
-        <!-- pic (Bootstrap MODAL) -->
-        <div class="modal fade" id="viewPic" role="dialog" >
-            <div class="modal-dialog">
-                <div class="modal-content container">
-                    <div class="modal-body">
-                        <?php  echo "<img src='payment/$_GET[viewPic]' style=width:300px;height:500px>";?>
+
+<body>
+
+    <div class="wrapper">
+        <!-- Sidebar  -->
+        <nav id="sidebar" class="bg-dark">
+            <div class="sidebar-header bg-dark">
+                <h3 class="mt-3">Admin</h3>
+            </div>
+            <ul class="list-unstyled components ms-3">
+                <li class="mb-2">
+                    <a href="#" id="pos">
+                        <i class="bi bi-tag me-2"></i>
+                        Point of Sales
+                    </a>
+                </li>
+                <li class="mb-2">
+                    <a href="#" id="orders">
+                        <i class="bi bi-minecart me-2"></i>
+                        Orders
+                    </a>
+                </li>
+                <li class="mb-2">
+                    <a href="#" id="ordersQueue">
+                        <i class="bi bi-clock me-2"></i>
+                        Orders Queue
+                    </a>
+                </li>
+                <li class="mb-2">
+                    <a href="#" id="inventory">
+                        <i class="bi bi-box-seam me-2"></i>
+                        Inventory
+                    </a>
+                </li>
+                <li class="mb-2">
+                    <a href="#" id="salesReport">
+                        <i class="bi bi-bar-chart me-2"></i>
+                        Sales Report
+                    </a>
+                </li>
+                <li class="mb-2">
+                    <a href="#" id="accountManagement">
+                        <i class="bi bi-person-circle me-2"></i>
+                        Account Management
+                    </a>
+                </li>
+                <li class="mb-2">
+                    <a href="#" id="customerFeedback">
+                        <i class="bi bi-chat-square-text me-2"></i>
+                        Customer Feedback
+                    </a>
+                </li>
+                <li class="mb-1 active">
+                    <a href="#">
+                        <i class="bi bi-cash-stack me-2"></i>
+                        Top-Up
+                    </a>
+                </li>
+                <li>
+                    <form method="post">
+                        <button class="btn btnLogout btn-dark text-danger" id="Logout" name="logout">
+                            <i class="bi bi-power me-2"></i>
+                            Logout
+                        </button>
+                    </form>
+                </li>
+            </ul>
+        </nav>
+
+        <!-- Page Content  -->
+        <div id="content">
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid bg-transparent">
+                    <button type="button" id="sidebarCollapse" class="btn" style="font-size:20px;">
+                        <i class="bi bi-list"></i>
+                        <span>Dashboard</span>
+                    </button>
+                </div>
+            </nav>
+            <!-- content here -->
+            <div class="container-fluid text-center">
+                <div class="row justify-content-center">
+                    <?php
+                        $query = "SELECT a.*,b.name FROM `WEBOMS_topUp_tb` a inner join WEBOMS_userInfo_tb b on a.user_id = b.user_id";
+                        $resultSet =  getQuery($query);
+                    ?>
+                    <div class="table-responsive col-lg-12">
+                        <table class="table table-bordered mb-5 col-lg-12">
+                            <thead>
+                                <tr>
+                                    <th scope="col">NAME</th>
+                                    <th scope="col">AMOUNT</th>
+                                    <th scope="col">STATUS</th>
+                                    <th scope="col">DATE & TIME</th>
+                                    <th scope="col">PROOF OF PAYMENT</th>
+                                    <th scope="col">ACTION</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                    if($resultSet!= null)
+                                    foreach($resultSet as $row){ ?>
+                                <tr>
+                                    <!-- name -->
+                                    <td><?php echo $row['name']; ?></td>
+                                    <!-- amount -->
+                                    <td><?php echo '₱'.$row['amount'];?></td>
+                                    <!-- status -->
+                                    <td><?php echo $row['status'];?></td>
+                                    <!-- date and time -->
+                                    <td><?php echo date('m/d/Y h:i a ', strtotime($row['date']));?></td>
+                                    <!-- proof of payment -->
+                                    <td>
+                                        <!-- <?php echo $row['proofOfPayment'];?> -->
+                                        <a class="btn btn-light border-dark"
+                                            href="?viewPic=<?php echo $row['proofOfPayment'];?>">
+                                            <i class="bi bi-list me-1"></i>
+                                            VIEW
+                                        </a>
+                                    </td>
+                                    <!-- action -->
+                                    <?php if($row['status'] == 'pending') {?>
+                                    <td>
+                                        <a class="btn btn-success"
+                                            href="?approve=<?php echo $row['id'].','.$row['user_id'].','.$row['amount'];?>">
+                                            <i class="bi bi-check me-1"></i>
+                                            APPROVE
+                                        </a>
+                                        <a class="btn btn-danger" href="?disapprove=<?php echo $row['id'];?>">
+                                            <i class="bi bi-x me-1"></i>
+                                            DISAPPROVE
+                                        </a>
+                                    </td>
+                                    <?php } else {?>
+                                    <td colspan="3">none</td>
+                                    <?php } ?>
+                                </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <!-- pic (Bootstrap MODAL) -->
+                    <div class="modal fade" id="viewPic" role="dialog">
+                        <div class="modal-dialog">
+                            <div class="modal-content container">
+                                <div class="modal-body">
+                                    <?php  echo "<img src='payment/$_GET[viewPic]' style=width:300px;height:500px>";?>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-	    </div>
-    </body>
+    </div>
+
+</body>
+
 </html>
 <?php 
-  //view pic
-  if(isset($_GET['viewPic'])){
-    echo "<script>$('#viewPic').modal('show');</script>";
-  }
-  //approve
-  if(isset($_GET['approve'])){
-    $arr = explode(',',$_GET['approve']);
-    $id = $arr[0];
-    $user_id = $arr[1];
-    $amount = $arr[2];
-    $query = "UPDATE WEBOMS_topUp_tb SET status='approved' WHERE id='$id' ";     
-    if(Query($query)){
-        $query = "UPDATE WEBOMS_userInfo_tb SET balance = (balance + '$amount') where user_id = '$user_id' ";     
-        if(Query($query)){
-            echo "<SCRIPT>  window.location.replace('adminTopUp.php'); alert('success!');</SCRIPT>";
-        }
+    //view pic
+    if(isset($_GET['viewPic'])){
+        echo "<script>$('#viewPic').modal('show');</script>";
     }
+    //approve
+    if(isset($_GET['approve'])){
+        $arr = explode(',',$_GET['approve']);
+        $id = $arr[0];
+        $user_id = $arr[1];
+        $amount = $arr[2];
+        $query = "UPDATE WEBOMS_topUp_tb SET status='approved' WHERE id='$id' ";     
+        if(Query($query)){
+            $query = "UPDATE WEBOMS_userInfo_tb SET balance = (balance + '$amount') where user_id = '$user_id' ";     
+            if(Query($query)){
+                echo "<SCRIPT>  window.location.replace('adminTopUp.php'); alert('success!');</SCRIPT>";
+            }
+        }
 
-  }
-  //disapprove
-  if(isset($_GET['disapprove'])){
-    $query = "UPDATE WEBOMS_topUp_tb SET status='disapproved' WHERE id = '$_GET[disapprove]' ";     
-    if(Query($query))
-        echo "<SCRIPT>  window.location.replace('adminTopUp.php'); alert('success!');</SCRIPT>";
-  }
+    }
+    //disapprove
+    if(isset($_GET['disapprove'])){
+        $query = "UPDATE WEBOMS_topUp_tb SET status='disapproved' WHERE id = '$_GET[disapprove]' ";     
+        if(Query($query))
+            echo "<SCRIPT>  window.location.replace('adminTopUp.php'); alert('success!');</SCRIPT>";
+    }
 ?>
+
+<script>
+// sidebar toggler
+$(document).ready(function() {
+    $('#sidebarCollapse').on('click', function() {
+        $('#sidebar').toggleClass('active');
+    });
+});
+</script>
+
+<script>
+// for navbar click locations
+document.getElementById("pos").onclick = function() {
+    window.location.replace('adminPos.php');
+};
+document.getElementById("orders").onclick = function() {
+    window.location.replace('adminOrders.php');
+};
+document.getElementById("ordersQueue").onclick = function() {
+    window.location.replace('adminOrdersQueue.php');
+};
+document.getElementById("inventory").onclick = function() {
+    window.location.replace('adminInventory.php');
+};
+document.getElementById("salesReport").onclick = function() {
+    window.location.replace('adminSalesReport.php');
+};
+document.getElementById("accountManagement").onclick = function() {
+    window.location.replace('accountManagement.php');
+};
+document.getElementById("customerFeedback").onclick = function() {
+    window.location.replace('customerFeedbackList.php');
+};
+// logout wala pa
+</script>
