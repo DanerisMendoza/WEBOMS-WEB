123835a8bb834a4dcdb4630d7f01afcc965c2320
dane <danerismendoza096@gmail.com>
2023-02-09 17:39:22 +0800

    check if account is logged in bug fix

--- method/checkIfAccountLoggedIn.php
+++ method/checkIfAccountLoggedIn.php
@@ -11,12 +11,29 @@ session_start();
 
     // account is already login
     if($page == 'notLogin' && $_SESSION['account'] == 'valid'){
-        if($_SESSION['accountType'] == 'customer')
-            die ("<script>window.location.replace('../customer/customer.php'); alert('Already Loggedin!');</script>");
-        else if($_SESSION['accountType'] == 'admin' || $_SESSION['accountType'] == 'manager')
-            die ("<script>window.location.replace('../nonCustomer/admin.php'); alert('Already Loggedin!');</script>");
+        if($_SESSION['accountType'] == 'customer'){
+            if($page != 'notLogin'){
+                die ("<script>window.location.replace('customer/customer.php'); alert('Already Loggedin!');</script>");
+            }
+            else{
+                die ("<script>window.location.replace('../customer/customer.php'); alert('Already Loggedin!');</script>");
+            }
+        }
+        else if($_SESSION['accountType'] == 'admin' || $_SESSION['accountType'] == 'manager'){
+            if($page != 'notLogin'){
+                die ("<script>window.location.replace('nonCustomer/admin.php'); alert('Already Loggedin!');</script>");
+            }
+            else{
+                die ("<script>window.location.replace('../nonCustomer/admin.php'); alert('Already Loggedin!');</script>");
+            }
+        }
         else if($_SESSION['accountType'] == 'cashier')
-            die ("<script>window.location.replace('../nonCustomer/pos/adminPos.php'); alert('Already Loggedin!');</script>");
+            if($page != 'notLogin'){
+                die ("<script>window.location.replace('nonCustomer/pos/adminPos.php'); alert('Already Loggedin!');</script>");
+            }
+            else{
+                die ("<script>window.location.replace('../nonCustomer/pos/adminPos.php'); alert('Already Loggedin!');</script>");
+            }
     }
 
     //account is not customer
