36e1b6f4d0c3682f8b66af79b72ae77ef8ffe25c
nicollette porca <20200108m.porca.nicollette.bscs@gmail.com>
2023-05-20 15:31:57 +0800

    customerTopUp.php and customer-top-up.css

--- customer/customerTopUp.php
+++ customer/customerTopUp.php
@@ -5,7 +5,7 @@
     $user_id = $_SESSION['user_id'];
     $query = "SELECT a.*,b.name FROM `weboms_topUp_tb` a inner join weboms_userInfo_tb b on a.user_id = b.user_id where a.user_id = '$user_id' order by a.id desc";
     $resultSet =  getQuery2($query);
-    //getting balance amount
+    // getting balance amount
     $query = "SELECT balance FROM `weboms_userInfo_tb` where user_id = '$_SESSION[user_id]' ";
     $balance = getQueryOneVal2($query,'balance');
     $balance = $balance == null ? 0 : $balance;
@@ -16,126 +16,120 @@
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Top Up</title>
 
-    <link rel="stylesheet" type="text/css" href="../css/bootstrap 5/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="../css/customer.css">
-    <script type="text/javascript" src="../js/jquery-3.6.1.min.js"></script>
-    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="../js/bootstrap 5/bootstrap.min.js"></script>
-    <!-- online css bootsrap icon -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
-    <!-- data table -->
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
-    <script type="text/javascript" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="../css/customer.css">
+    <link rel="stylesheet" href="../css/customer-top-up.css">
 
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.5.3/js/bootstrap.min.js"></script>
 </head>
+<body>
 
-<body style="background:#e0e0e0">
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top shadow">
-        <div class="container py-3">
-            <a class="navbar-brand fs-4" href="#"><?php echo $companyName;?></a>
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container">
+            <a class="navbar-brand animate__animated animate__fadeInLeft" href="#"><?php echo strtoupper($companyName); ?></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item me-2">
-                        <a class="nav-link text-dark" href="customer.php"><i class="bi bi-house-door"></i> HOME</a>
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link animate__animated animate__fadeInLeft" href="customer.php">HOME</a>
                     </li>
-                    <li class="nav-item me-2">
-                        <a class="nav-link text-dark" href="customerProfile.php"><i class="bi bi-person-circle"></i> PROFILE</a>
+                    <li class="nav-item">
+                        <a class="nav-link animate__animated animate__fadeInLeft" href="customerProfile.php">PROFILE</a>
                     </li>
-                    <li class="nav-item me-2">
-                        <a class="nav-link text-dark" href="customerMenu.php"><i class="bi bi-book"></i> MENU</a>
+                    <li class="nav-item">
+                        <a class="nav-link animate__animated animate__fadeInLeft" href="customerMenu.php">MENU</a>
                     </li>
-                    <li class="nav-item me-2">
-                        <a class="nav-link text-danger" href="customerTopUp.php"><i class="bi bi-cash-stack"></i> TOP-UP</a>
+                    <li class="nav-item">
+                        <a class="nav-link text-danger animate__animated animate__fadeInLeft" href="customerTopUp.php">TOP-UP</a>
                     </li>
-                    <li class="nav-item me-2">
-                        <a class="nav-link text-dark" href="customerOrders.php"><i class="bi bi-list"></i> VIEW ORDERS</a>
+                    <li class="nav-item">
+                        <a class="nav-link animate__animated animate__fadeInLeft" href="customerOrders.php">VIEW ORDERS</a>
                     </li>
                 </ul>
-                <form method="post">
-                    <button class="btn btn-danger" id="Logout" name="logout"><i class="bi bi-power"></i> LOGOUT</button>
+                <form action="" method="post">
+                    <button class="btn btn-logout btn-outline-light animate__animated animate__fadeInLeft" id="Logout" name="logout">LOGOUT</button>
                 </form>
             </div>
         </div>
     </nav>
 
-    <div class="container text-center" style="margin-top:130px;">    
-        <div class="row">
-            <div class="container mb-5 shadow">
-                <div class="row g-5">
-                    <div class="col-sm-4 bg-white"> 
-                        <div class="container py-4">
-                            <!-- content here -->
-                            <h2 class="fw-normal mt-3 mb-4">Top-Up</h2>
-                            <form method="post" enctype="multipart/form-data" class="col-12">
-                                <select name="amount" class="form-control form-control-lg col-12 mb-4">
-                                    <option value="100">₱100.00</option>
-                                    <option value="300">₱300.00</option>
-                                    <option value="500">₱500.00</option>
-                                    <option value="1000">₱1000.00</option>
-                                    <option value="3000">₱3000.00</option>
-                                    <option value="5000">₱5000.00</option>
-                                </select>
-                                <h5 class="fw-normal">PROOF OF PAYMENT:</h5>
-                                <input type="file" class="form-control form-control-lg mb-4" name="fileInput" required>
-                                <button class="btn btn-lg btn-success col-12 mb-3" name="submit"><i class="bi bi-cash-stack"></i> Top-Up</button>
-                            </form>
-                        </div>
+    <div class="container">
+        <div class="card top-up-card">
+            <div class="col-sm-12">
+                <div class="row">
+                    <div class="col-sm-4 colum-left animate__animated animate__fadeInLeft">
+                        <center><label for="" class="top-up">TOP-UP</label></center>
+                        <form method="post" enctype="multipart/form-data">
+                            <select name="amount" class="form-control">
+                                <option value="100">₱100.00</option>
+                                <option value="300">₱300.00</option>
+                                <option value="500">₱500.00</option>
+                                <option value="1000">₱1000.00</option>
+                                <option value="3000">₱3000.00</option>
+                                <option value="5000">₱5000.00</option>
+                            </select>
+                            <label for="">PROOF OF PAYMENT:</label>
+                            <input type="file" class="form-control" name="fileInput" required>
+                            <button class="btn btn-submit btn-success" name="submit">SUBMIT</button>
+                        </form> 
                     </div>
-                    <div class="col-sm-8 bg-white">
-                        <div class="container py-4">
-                            <!-- content here -->
-                            <h2 class="mt-3 mb-4 bg-success text-white">Your balance is ₱<?php echo number_format($balance,2);?></h2>
-                            <div class="table-responsive">
-                                <table class="table table-hover table-bordered col-lg-12" id="tb1">
-                                    <thead class="table-dark">
-                                        <tr>
-                                            <th scope="col">NAME</th>
-                                            <th scope="col">AMOUNT</th>
-                                            <th scope="col">STATUS</th>
-                                            <th scope="col">DATE & TIME<br>(MM/DD/YYYY)</th>
-                                            <th scope="col">PAYMENT</th>
-                                            <th scope="col">ACTION</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php
+                    <div class="col-sm-8 animate__animated animate__fadeInLeft">
+                        <center><label for="" class="balance text-success">BALANCE: P<?php echo number_format($balance, 2); ?></label></center>
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-striped" id="tb1">
+                                <thead>
+                                    <tr>
+                                        <th>NAME</th>
+                                        <th>AMOUNT</th>
+                                        <th>STATUS</th>
+                                        <th>DATE-TIME (MM/DD/YYYY)</th>
+                                        <th>PAYMENT</th>
+                                        <th>ACTIONS</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
                                         if($resultSet!= null)
                                         foreach($resultSet as $row){ ?>
                                         <tr>
-                                            <td><?php echo ucwords($row['name']); ?></td>
-                                            <td><?php echo '₱'. number_format($row['amount'],2);?></td>
-                                            <td><?php echo ucwords($row['status']);?></td>
+                                            <td><?php echo strtoupper($row['name']); ?></td>
+                                            <td><?php echo '₱'.number_format($row['amount'],2);?></td>
+                                            <td><?php echo strtoupper($row['status']);?></td>
                                             <td><?php echo date('m/d/Y h:i a ', strtotime($row['date']));?></td>
                                             <?php if($row['status'] != 'approved' && $row['status'] != 'disapproved'){?>
                                             <td>
-                                                <a class="btn btn-light" style="border:1px solid #cccccc;" href="?viewPic=<?php echo $row['proofOfPayment'];?>"><i class="bi bi-list"></i> View</a>
+                                                <a class="btn btn-light" href="?viewPic=<?php echo $row['proofOfPayment'];?>"><i class="bi-list"></i></a>
                                             </td>
                                             <td>
-                                                <a class="btn btn-danger" href="?cancel=<?php echo $row['id'].','.$row['proofOfPayment'];?>"><i class="bi bi-x-circle"></i> Cancel</a>
+                                                <a class="btn btn-danger" href="?cancel=<?php echo $row['id'].','.$row['proofOfPayment'];?>"><i class="bi-x"></i></a>
                                             </td>
                                             <?php }else if($row['proofOfPayment'] != ''){ ?>
-                                            <td><a class="btn btn-light" style="border:1px solid #cccccc;" href="?viewPic=<?php echo $row['proofOfPayment'];?>"><i class="bi bi-list"></i> View</a></td>
+                                            <td><a class="btn btn-light" href="?viewPic=<?php echo $row['proofOfPayment'];?>"><i class="bi-list"></i></a></td>
                                             <td></td>
                                             <?php }else { ?>
-                                            <td>Payment Through RFID</td>
+                                            <td>PAYMENT THROUGH RFID</td>
                                             <td></td>
                                             <?php } ?>
                                         </tr>
-                                        <?php } ?>
-                                    </tbody>
-                                </table>
-                            </div>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
@@ -144,18 +138,17 @@
     </div>
 
     <!-- pic (Bootstrap MODAL) -->
-    <div class="modal fade" id="viewPic" role="dialog">
+    <div class="modal" tabindex="-1" id="viewPic" role="dialog">
         <div class="modal-dialog">
-            <div class="modal-content container text-center">
+            <div class="modal-content">
                 <div class="modal-body">
-                    <?php  echo "<img src='../payment/$_GET[viewPic]' style=width:300px;height:550px>";?>
+                    <center><?php echo "<img src='../payment/$_GET[viewPic]' class='proof-img'";?></center>
                 </div>
             </div>
         </div>
     </div>
 
 </body>
-
 </html>
 
 <?php 
@@ -213,18 +206,19 @@
 ?>
 
 <?php 
-  if(isset($_POST['logout'])){
-    session_destroy();
-    echo "<script>window.location.replace('../general/login.php');</script>";
-  }
+    if(isset($_POST['logout'])){
+        session_destroy();
+        echo "<script>window.location.replace('../general/login.php');</script>";
+    }
 ?>
+
 <script>
     $(document).ready(function() {
         $('#tb1').DataTable();
     });
     $('#tb1').dataTable({
-    "columnDefs": [
-        { "targets": [4,5], "orderable": false }
-    ]
+        "columnDefs": [
+            { "targets": [4,5], "orderable": false }
+        ]
     });
 </script>
