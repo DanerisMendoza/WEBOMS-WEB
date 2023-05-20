1448b69174389fcede870fa5fa7f339a945c9955
nicollette porca <20200108m.porca.nicollette.bscs@gmail.com>
2023-05-20 16:36:24 +0800

    customerFeedBack.php and customer-feedback.css

--- customer/customerFeedBack.php
+++ customer/customerFeedBack.php
@@ -11,60 +11,59 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>View Orders - Feedback</title>
+    <title>View Orders | Feedback</title>
 
-    <link rel="stylesheet" type="text/css" href="../css/bootstrap 5/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/customer.css"> 
-    <!-- online css bootsrap icon -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <link rel="stylesheet" href="../css/customer.css">
+    <link rel="stylesheet" href="../css/customer-feedback.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.min.js"></script>
 </head>
-<body style="background:#e0e0e0">
+<body>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top shadow">
-        <div class="container py-3">
-            <a class="navbar-brand fs-4" href="#"><?php echo $companyName;?></a>
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container">
+            <a class="navbar-brand animate__animated animate__fadeInLeft" href="#"><?php echo strtoupper($companyName); ?></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item me-2">
-                        <a class="nav-link text-dark" href="customer.php"><i class="bi bi-house-door"></i> HOME</a>
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link animate__animated animate__fadeInLeft" href="customer.php">HOME</a>
                     </li>
-                    <li class="nav-item me-2">
-                        <a class="nav-link text-dark" href="customerProfile.php"><i class="bi bi-person-circle"></i> PROFILE</a>
+                    <li class="nav-item">
+                        <a class="nav-link animate__animated animate__fadeInLeft" href="customerProfile.php">PROFILE</a>
                     </li>
-                    <li class="nav-item me-2">
-                        <a class="nav-link text-dark" href="customerMenu.php"><i class="bi bi-book"></i> MENU</a>
+                    <li class="nav-item">
+                        <a class="nav-link animate__animated animate__fadeInLeft" href="customerMenu.php">MENU</a>
                     </li>
-                    <li class="nav-item me-2">
-                        <a class="nav-link text-dark" href="customerTopUp.php"><i class="bi bi-cash-stack"></i> TOP-UP</a>
+                    <li class="nav-item">
+                        <a class="nav-link animate__animated animate__fadeInLeft" href="customerTopUp.php">TOP-UP</a>
                     </li>
-                    <li class="nav-item me-2">
-                        <a class="nav-link text-danger" href="customerOrders.php"><i class="bi bi-list"></i> VIEW ORDERS</a>
-                    </li>
-                    <li>
-                        <form method="post">
-                            <button class="btn btn-danger col-12" id="Logout" name="logout"><i class="bi bi-power"></i> LOGOUT</button>
-                        </form>
+                    <li class="nav-item">
+                        <a class="nav-link text-danger animate__animated animate__fadeInLeft" href="customerOrders.php">VIEW ORDERS</a>
                     </li>
                 </ul>
+                <form action="" method="post">
+                    <button class="btn btn-logout btn-outline-light animate__animated animate__fadeInLeft" id="Logout" name="logout">LOGOUT</button>
+                </form>
             </div>
         </div>
     </nav>
-    
-<div class="container text-center bg-white shadow p-5" style="margin-top:130px;">
-    <div class="row justify-content-center">
-        <div class="col-lg-12">
-            <button class="btn btn-lg btn-dark col-12 mb-4" id="orderList"><i class="bi bi-arrow-left-short"></i> Back</button>
-            <script>document.getElementById("orderList").onclick = function () {window.location.replace('customerOrders.php'); }; </script> 
-            <form method="post">
-                <textarea type="text" name="feedback" placeholder="Enter your feedback" class="form-control form-control-lg mb-3" rows="5" required></textarea>
-                <button type="submit" name="submit" class="btn btn-lg btn-success col-12"><i class="bi bi-file-arrow-up"></i> Submit</button>
-            </form>
+
+    <div class="container">
+        <div class="card feedback-card">
+            <a href="customerOrders.php" class="back-menu"><i class="bi-arrow-left"></i>BACK TO ORDERS</a>
+            <div class="card feedback-card2">
+                <form action="" method="post">
+                    <textarea name="feedback" id="" cols="" rows="10" type="text" placeholder="Enter your feedback" class="form-control" required></textarea>
+                    <button type="submit" name="submit" class="btn btn-success">SUBMIT</button>
+                </form>
+            </div>
         </div>
-	</div>
-</div>
+    </div>
     
 </body>
 </html>
