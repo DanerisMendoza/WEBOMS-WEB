7462b30d887eb803adf6e0428c43b47eec7fd0b5
Daneris <danerismendoza096@gmail.com>
2023-05-20 13:53:22 +0800

    receipt layout fix for thermal printer

--- pdf/receipt.php
+++ pdf/receipt.php
@@ -26,66 +26,69 @@
     $pdf->SetCreator(PDF_CREATOR);
     $pdf->SetTitle("Receipt");
     // $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
-    $pdf->SetMargins(1,1,1);
+    $pdf->SetMargins(0,0,0);
     $pdf->setPrintHeader(false);
     $pdf->setPrintFooter(false);
-    $pdf->SetAutoPageBreak(TRUE, 1);
+    $pdf->SetAutoPageBreak(TRUE, 0);
 
     $pdf->AddPage('P','A6');
-    $pdf->SetFont('pdfahelvetica', 'B', 40);
+    $pdf->SetFont('pdfahelvetica', 'B', 25);
     $pdf -> Cell(0,15,"ORDER#$_SESSION[order_id]", 0, 1,'C');
 
-    $pdf->SetFont('pdfahelvetica', '', 20);
+    $pdf->SetFont('pdfahelvetica', '', 13);
     $pdf -> Cell(0,5,"$_SESSION[companyName]", 0, 1,'C');
     $pdf -> Cell(0,5,"$_SESSION[companyAddress]", 0, 1,'C');
     $pdf -> Cell(0,5,"$_SESSION[companyTel]", 0, 1,'C');
     $pdf -> ln(3);
     $pdf -> Cell(0,5,"$date", 0, 1,'C');
-    $pdf -> ln(5);
-
-    $pdf -> Cell(20,10,"QTY",'B,T', 0,'C');
-    $pdf -> Cell(40,10,"DISH",'B,T', 0,'L');
-    $pdf -> Cell(0,10,"PRICE",'B,T', 0,'R');
-    $pdf -> ln(10);
+    $pdf -> ln(3);
+    $pdf->SetFont('pdfahelvetica', '', 14);
+    $pdf -> Cell(37,10,"Dish",'B,T', 0,'L');
+    $pdf -> Cell(20,10,"Qty.",'B,T', 0,'C');
+    $pdf -> Cell(23,10,"Price",'B,T', 0,'R');
+    $pdf -> Cell(0,10,"Sub Total",'B,T', 0,'R');
+    $pdf -> ln(9);
     for($i=0; $i<count($dishesArr); $i++){
         $price = number_format($priceArr[$i],2);
+        $pdf -> Cell(37,10,"$dishesArr[$i]",'', 0,'L');
         $pdf -> Cell(20,10,"$dishesQuantity[$i]",'',0,'C');
-        $pdf -> Cell(40,10,"$dishesArr[$i]",'', 0,'L');
+        $origPrice = number_format($priceArr[$i]/$dishesQuantity[$i],2);
+        $pdf -> Cell(23,10,"$origPrice",'',0,'R');
         $pdf -> Cell(0,10,"$price",'',0,'R');
-        $pdf -> ln(5);
+        $pdf -> ln(6);
     }
-    $pdf -> ln(8);
+    $pdf -> ln(4);
 
-    $pdf->SetFont('pdfahelvetica', 'B', 30);
+    $pdf->SetFont('pdfahelvetica', 'B', 25);
     $pdf -> Cell(70,10,"TOTAL",'T','0','L');
     $pdf -> Cell(33,10,"P$total",'T','0','R');
-    $pdf -> ln(10);
-    $pdf->SetFont('pdfahelvetica', '', 20);
-    $pdf -> Cell(70,10,"PAYMENT",'','0','L');
+    $pdf -> ln(8);
+    $pdf->SetFont('pdfahelvetica', '', 15);
+    $pdf -> Cell(70,10,"Payment",'','0','L');
     $pdf -> Cell(33,10,"P$cash",'','0','R');
     $pdf -> ln(6);
-    $pdf -> Cell(70,10,"CHANGE",'','0','L');
-    $pdf -> Cell(33,10,"P$change",'','0','R');
-    $pdf -> ln(11);
+    $pdf -> Cell(70,10,"Change",'B','0','L');
+    $pdf -> Cell(33,10,"P$change",'B','0','R');
+    $pdf -> ln(9);
 
-    $pdf -> Cell(0,10,"CUSTOMER: $_SESSION[customerName]" ,'T','0','L');
-    $pdf -> ln(7);
+    $pdf -> Cell(0,10,"Customer: $_SESSION[customerName]" ,'','0','L');
+    $pdf -> ln(6);
     if($_SESSION['staffInCharge'] == 'online order'){
-        $pdf -> Cell(0,10,"ORDER TYPE: ONLINE ORDER",'','0','L');
+        $pdf -> Cell(0,10,"Order Type: ONLINE ORDER",'','0','L');
     }
     else{
-        $pdf -> Cell(0,10,"ORDER TYPE: POS",'','0','L');
+        $pdf -> Cell(0,10,"Order Type: POS",'','0','L');
     }
-    $pdf -> ln(7);
-    $pdf -> Cell(0,10,"ORDER NO.: $_SESSION[or_number] ",'','0','L');
-    $pdf -> ln(7);
+    $pdf -> ln(6);
+    $pdf -> Cell(0,10,"Order No.: $_SESSION[or_number] ",'','0','L');
+    $pdf -> ln(6);
     if($_SESSION['staffInCharge'] != 'online order'){
-        $pdf -> Cell(0,10,"CASHIER: $_SESSION[name]",'','0','L');
+        $pdf -> Cell(0,10,"Cashier: $_SESSION[name]",'','0','L');
     }
-    $pdf -> ln(12);
+    $pdf -> ln(9);
 
     // $pdf->SetFont('dejavusans', '', 13);
-    $pdf -> Cell(0,10,"Thank you. Please come again.", 'B,T', 1,'C');
+    $pdf -> Cell(0,10,"Thank you. Please come again!", 'B,T', 1,'C');
 
     ob_end_clean();
     $pdf->Output('file.pdf', 'I');
