f072283bc615ecebba98a65d7439d63d9f74fadb
nicollette porca <20200108m.porca.nicollette.bscs@gmail.com>
2023-05-22 18:16:59 +0800

    admin.php, admin.css, and admin-dashboard.css

--- nonCustomer/admin3.php
+++ nonCustomer/admin3.php
@@ -6,165 +6,193 @@
     // redefining name
     $_SESSION['name'] = getQueryOneVal2("select name from weboms_userInfo_tb where user_id = '$_SESSION[user_id]' ",'name');
 
-    // current week total sold
+    //  current week total sold
     $todaySold = 0; 
     $dailySoldMultiArr = $weeklySoldMultiArr = $monthlySoldMultiArr =  $yearlySoldMultiArr = [[],[]]; 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en"> 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Admin</title>
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="../css/admin.css">
-    <link rel="stylesheet" href="../css/admin-dashboard.css">
-    <link rel="icon" href="../image/weboms.png">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
-    <script src="https://www.gstatic.com/charts/loader.js"></script>
+    <link rel="stylesheet" href="../css/bootstrap 5/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/admin2.css">
+    <script type="text/javascript" src="../js/bootstrap 5/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../js/jquery-3.6.1.min.js"></script>
+    <!-- online css bootsrap icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+    <!-- charts -->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+
+    <!-- <style>
+        .h1Admin {
+            font-size: 13vw;
+            font-weight: normal;
+        }
+        .chart {
+            width: 100%; 
+            min-height: 450px;
+        }
+        .row {
+            margin:0 !important;
+        }
+    </style> -->
 </head>
-<body>
 
+<body> 
     <div class="wrapper">
-        <nav id="sidebar">
-            <div class="sidebar-header">
-                <a href="admin.php" class="account-type"><?php echo strtoupper($_SESSION['accountType']); ?></a>
+        <!-- Sidebar  -->
+        <nav id="sidebar" class="bg-dark">
+            <div class="sidebar-header bg-dark">
+                <h3 class="mt-3"><a href="admin.php"><?php echo ucwords($_SESSION['accountType']); ?></a></h3>
             </div>
-            <hr>
-            <ul class="list-unstyled components">
-                <li><a href="adminPos.php"><i class="bi-tag me-2"></i>POINT OF SALES</a></li>
-                <li><a href="adminOrders.php"><i class="bi-cart me-2"></i>ORDERS</a></li>
-                <li><a href="adminOrdersQueue.php"><i class="bi-clock me-2"></i>ORDERS QUEUE</a></li>
-                <li><a href="topupRfid.php"><i class="bi-credit-card me-2"></i>TOP-UP RFID</a></li>
+            <ul class="list-unstyled components ms-3">
+                <li class="mb-2">
+                    <a href="adminPos.php"><i class="bi bi-tag me-2"></i>Point of Sales</a>
+                </li>
+                <li class="mb-2">
+                    <a href="adminOrders.php"><i class="bi bi-minecart me-2"></i>Orders</a>
+                </li>
+                <li class="mb-2">
+                    <a href="adminOrdersQueue.php"><i class="bi bi-clock me-2"></i>Orders Queue</a>
+                </li>
 
-                <?php if($_SESSION['accountType'] != 'cashier'){?>
-                <li><a href="adminTopUp.php"><i class="bi-wallet me-2"></i>TOP-UP</a></li>
-                <li><a href="adminInventory.php"><i class="bi-box me-2"></i>INVENTORY</a></li>
-                <li><a href="adminSalesReport.php"><i class="bi-bar-chart me-2"></i>SALES REPORT</a></li>
-                <li><a href="adminFeedbackList.php"><i class="bi-chat-square-text me-2"></i>CUSTOMER FEEDBACK</a></li>
-                <li><a href="accountManagement.php"><i class="bi-person me-2"></i>ACCOUNT MANAGEMENT</a></li>
-                <li><a href="settings.php"><i class="bi-gear me-2"></i>SETTINGS</a></li>
-                <?php } ?>
+                <li class="mb-2">
+                    <a href="topupRfid.php"><i class="bi bi-credit-card me-2"></i>Top-Up RFID</a>
+                </li>
+            
+            <?php if($_SESSION['accountType'] != 'cashier'){?>
+                <li class="mb-2">
+                    <a href="adminInventory.php"><i class="bi bi-box-seam me-2"></i>Inventory</a>
+                </li>
+                <li class="mb-2">
+                    <a href="adminSalesReport.php"><i class="bi bi-bar-chart me-2"></i>Sales Report</a>
+                </li>
+                <li class="mb-2">
+                    <a href="accountManagement.php"><i class="bi bi-person-circle me-2"></i>Account Management</a>
+                </li>
+                <li class="mb-2">
+                    <a href="adminFeedbackList.php"><i class="bi bi-chat-square-text me-2"></i>Customer Feedback</a>
+                </li>
+                <li class="mb-2">
+                    <a href="adminTopUp.php"><i class="bi bi-cash-stack me-2"></i>Top-Up</a>
+                </li>
+                <li class="mb-1">
+                    <a href="settings.php"><i class="bi bi-gear me-2"></i>Settings</a>
+                </li>
+            <?php } ?>
+                <li>
+                    <form method="post">
+                        <button class="btn btnLogout btn-dark text-danger" id="Logout" name="logout"><i class="bi bi-power me-2"></i>Logout</button>
+                    </form>
+                </li>
             </ul>
         </nav>
 
+        <!-- Page Content  -->
         <div id="content">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <button type="button" id="sidebarCollapse" class="btn btn-toggle">
-                        <i class="bi-list"></i>
-                    </button>
-                    <button class="btn btn-toggle d-inline-block d-lg-none ml-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <i class="bi-list text-danger"></i>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="nav navbar-nav ms-auto">
-                            <li>
-                                <form method="post">
-                                    <button class="btn text-danger" id="Logout" name="logout">LOGOUT</button>
-                                </form>
-                            </li>   
-                        </ul>
-                    </div>
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid bg-transparent">
+                    <button type="button" id="sidebarCollapse" class="btn" style="font-size:20px;"><i class="bi bi-list"></i> Dashboard</button>
                 </div>
             </nav>
-
-            <div class="container-fluid mt-3">
-                <div class="col-sm-12">
+            <!-- content here -->
+            <div class="container-fluid text-center ">
+                <div class="row justify-content-center">
+                    <div class="table-responsive col-lg-4 mb-4">
+                        <table class="table table-bordered table-hover col-lg-12">
+                            <tr>
+                                <td><b>Total Amount of Stock:</b></td>
+                                <td id="totalStockTd"></td>
+                            </tr>
+                            <tr>
+                                <td><b>Total Count of Stock Left:</b></td>
+                                <td id="stockLeftTd"></td>
+                            </tr>
+                            <tr>
+                                <td><b>Total Count of Sold:</b></td>
+                                <td id="soldCountTd"></td>
+                            </tr>
+                            <tr>
+                                <td><b>Total Sold:</b></td>
+                                <td id="soldTd"></td>
+                            </tr>
+                        </table>
+                    </div> 
+                    <div class="table-responsive col-lg-4 mb-4">
+                        <table class="table table-bordered table-hover col-lg-12">
+                            <tr>
+                                <td><b>Total Count of Preparing:</b></td>
+                                <td id="preparingTd"></td>
+                            </tr>
+                            <tr>
+                                <td><b>Total Count of Serving:</b></td>
+                                <td id="servingTd"></td>
+                            </tr>
+                        </table>
+                    </div> 
+                    <div class="table-responsive col-lg-4 mb-4">
+                        <table class="table table-bordered table-hover col-lg-12">
+                            <tr>
+                                <td><b>Today Sale:</b></td>
+                                <td id="todaySaleTd"></td>
+                            </tr>
+                            <tr>
+                                <td><b>Current Week Sale:</b></td>
+                                <td id="currentWeekSoldTd"></td>
+                            </tr>
+                            <tr>
+                                <td><b>Current Month Sale:</b></td>
+                                <td id="currentMonthSoldTd"></td>
+                            </tr>
+                            <tr>
+                                <td><b>Current Year Sale:</b></td>
+                                <td id="currentYearSoldTd"></td>
+                            </tr>
+                        </table>
+                    </div>
                     <div class="row">
-                        <div class="col-sm-4">
-                            <table class="table table-bordered">
-                                <tr>
-                                    <td>TOTAL AMOUNT OF STOCK:</td>
-                                    <td id="totalStockTd"></td>
-                                </tr>
-                                <tr>
-                                    <td>TOTAL COUNT OF STOCK LEFT:</td>
-                                    <td id="stockLeftTd"></td>
-                                </tr>
-                                <tr>
-                                    <td>TOTAL COUNT OF SOLD:</td>
-                                    <td id="soldCountTd"></td>
-                                </tr>
-                                <tr>
-                                    <td>TOTAL SOLD:</td>
-                                    <td id="soldTd"></td>
-                                </tr>
-                            </table>
+                        <div class="col-md-6 mb-4">
+                            <div class="chart" id="piechart"></div>
                         </div>
-                        <div class="col-sm-4">
-                            <table class="table table-bordered">
-                                <tr>
-                                    <td>TOTAL COUNT OF PREPARING:</td>
-                                    <td id="preparingTd"></td>
-                                </tr>
-                                <tr>
-                                    <td>TOTAL COUNT OF SERVING:</td>
-                                    <td id="servingTd"></td>
-                                </tr>
-                            </table>
-                        </div>
-                        <div class="col-sm-4">
-                            <table class="table table-bordered">
-                                <tr>
-                                    <td>TODAY SALE:</td>
-                                    <td id="todaySaleTd"></td>
-                                </tr>
-                                <tr>
-                                    <td>CURRENT WEEK SALE:</td>
-                                    <td id="currentWeekSoldTd"></td>
-                                </tr>
-                                <tr>
-                                    <td>CURRENT MONTH SALE:</td>
-                                    <td id="currentMonthSoldTd"></td>
-                                </tr>
-                                <tr>
-                                    <td>CURRENT YEAR SALE:</td>
-                                    <td id="currentYearSoldTd"></td>
-                                </tr>
-                            </table>
+                        <div class="col-md-6 mb-4">
+                            <div class="chart" id="columnchart"></div> 
                         </div>
                     </div>
-                    <div class="table-responsive">
-                        <div class="row g-2">
-                            <div class="col-sm-6">
-                                <div class="chart" id="piechart"></div>
-                            </div>
-                            <div class="col-sm-6">
-                                <div class="chart" id="columnchart"></div>
-                            </div>
+
+                    <div class="row">
+                        <div class="col-md-6 mb-4">
+                            <div class="chart" id="columnchart2"></div> 
                         </div>
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <div class="chart" id="columnchart2"></div>
-                            </div>
-                            <div class="col-sm-6">
-                                <div class="chart" id="columnchart3"></div>
-                            </div>
+                        <div class="col-md-6 mb-4">
+                            <div class="chart" id="columnchart3"></div> 
                         </div>
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <div class="chart" id="columnchart4"></div>
-                            </div>
-                            <div class="col-sm-6">
-                                <div class="chart" id="columnchart5"></div>
-                            </div>
+                        
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-6 mb-4">
+                            <div class="chart" id="columnchart4"></div> 
+                        </div>
+                        <div class="col-md-6 mb-4">
+                            <div class="chart" id="columnchart5"></div> 
                         </div>
                     </div>
+
                 </div>
             </div>
         </div>
     </div>
-    
 </body>
-</html>
 
+</html>
 <script>
+
     // sidebar toggler
     $(document).ready(function() {
         $('#sidebarCollapse').on('click', function() {
@@ -173,14 +201,17 @@
     });
     
     $(document).ready(function() {
-        // Load the Visualization API and the corechart package
-        google.charts.load('current', {'packages':['corechart']});
-        // google.charts.load('current', {'packages':['bar']});
-        // Set a callback to run when the Visualization API is loaded
-        setInterval(function() {
-            google.charts.setOnLoadCallback(updateStats);
-        }, 1000);
+    // Load the Visualization API and the corechart package
+    google.charts.load('current', {'packages':['corechart']});
+    // google.charts.load('current', {'packages':['bar']});
+
+    // Set a callback to run when the Visualization API is loaded
+
+    setInterval(function() {
+        google.charts.setOnLoadCallback(updateStats);
+    }, 1000);
     });
+
 
     function updateStats(){
         $.getJSON({
@@ -314,6 +345,10 @@
             }
         });
     }
+    
+   
+
+    
 </script>
 
 <?php 
